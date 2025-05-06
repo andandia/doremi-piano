2cb129dd7d4a88f3bf57bfb2013e0d5b3cce0d3c
marmooo <marmooo@users.noreply.github.com>
2025-05-06 12:47:47 +0900

    bump bootstrap from 5.3.5 to 5.3.6

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "2025-04-06 01:35";
+const CACHE_NAME = "2025-05-06 12:35";
 const urlsToCache = [
   "/doremi-piano/",
   "/doremi-piano/index.js",
   "/doremi-piano/abt.mid",
   "/doremi-piano/instruments.lst",
   "/doremi-piano/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.worklet.min.js",
